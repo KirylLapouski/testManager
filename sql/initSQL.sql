2fdc628f75b900e6d10ec4b1c63213130c1ac308
Kirill Lapkovsky <lapkovskyk@mail.ru>
2018-04-10 15:29:18 +0300

    loopback models and relations

--- sql/initSQL.sql
+++ sql/initSQL.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS testManager;
-CREATE DATABASE testManager;
-use testManager;
+DROP DATABASE IF EXISTS testManager1;
+CREATE DATABASE testManager1;
+use testManager1;
 
 CREATE TABLE disciplines(
     id int unsigned not null auto_increment,
@@ -16,14 +16,23 @@ CREATE TABLE lessons (
     CONSTRAINT pk_lessons PRIMARY KEY (id),
     CONSTRAINT fk_lessons_discipline FOREIGN KEY(id_discipline) REFERENCES disciplines(id)
 );
+CREATE TABLE topics(
+    id int unsigned not null auto_increment,
+    path VARCHAR(255) not null,
+    type VARCHAR(10) not null,
+    id_lesson int unsigned not null, 
+
+    CONSTRAINT pk_topic PRIMARY KEY(id),
+    CONSTRAINT fk_topic_lesson FOREIGN KEY(id_lesson) REFERENCES lessons(id)
+);
 CREATE TABLE questions (
     id int unsigned not null auto_increment,
     title VARCHAR(200) not null,
     question_description VARCHAR(255),
-    id_lesson int unsigned not null,
+    id_topics int unsigned not null,
 
     CONSTRAINT pk_questions PRIMARY KEY (id),
-    CONSTRAINT fk_question_lesson FOREIGN KEY(id_lesson) REFERENCES lessons(id)
+    CONSTRAINT fk_question_topics FOREIGN KEY(id_topics) REFERENCES topics(id)
 );
 CREATE TABLE answers(
     id int unsigned not null auto_increment,
@@ -34,13 +43,4 @@ CREATE TABLE answers(
 
     CONSTRAINT pk_answers PRIMARY KEY(id),
     CONSTRAINT fk_answer_question FOREIGN KEY(id_question) REFERENCES questions(id)
-);
-CREATE TABLE topics(
-    id int unsigned not null auto_increment,
-    path VARCHAR(255) not null,
-    type VARCHAR(10) not null,
-    id_lesson int unsigned not null, 
-
-    CONSTRAINT pk_topic PRIMARY KEY(id),
-    CONSTRAINT fk_topic_lesson FOREIGN KEY(id_lesson) REFERENCES lessons(id)
 );
