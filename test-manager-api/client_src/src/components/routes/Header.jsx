699967335ca0991c1d8d3008ae11848581831769
KirylLapouski <lapkovskyk@mail.ru>
2018-05-08 10:06:53 +0300

    devide routes

---
+++ test-manager-api/client_src/src/components/routes/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Route,Switch,BrowserRouter as Router} from 'react-router-dom';
+import NavBarCustom from '../NavBarCustom';
+class Header extends React.Component{
+    render(){
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/" exact render={()=>{ return null}}/>  
+                    <Route path="/signUp" exact render={()=>{ return null}}/>
+                    <Route path="/" component={NavBarCustom} />           
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default Header
--- test-manager-api/client_src/src/components/routes/Main.jsx
+++ test-manager-api/client_src/src/components/routes/Main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import CourseContainer from './CourseContainer';
-import LessonContainer from './LessonContainer';
-import TopicContainer from './TopicContainer';
+import CourseContainer from '../CourseContainer';
+import LessonContainer from '../LessonContainer';
+import TopicContainer from '../TopicContainer';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
-import TestContainer from './testCMS/TestContainer.jsx';
+import TestContainer from '../testCMS/TestContainer.jsx';
+import LoginIn from '../LoginIn';
+import SignUp from '../SignUp';
+
 class Main extends React.Component {
     render() {
         return (
@@ -14,6 +17,8 @@ class Main extends React.Component {
                     <Route path="/:courseId/lessons" component={LessonContainer} />
                     <Route path="/lesson/:lessonId/topic/:topicId" component={TopicContainer} />
                     <Route path="/lesson/:lessonId/topics" component={TopicContainer} />
+                    <Route path="/" exact component={LoginIn}/>
+                    <Route path="/signUp" component={SignUp} />
                 </Switch>
             </Router>)
     }
