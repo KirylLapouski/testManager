fe64c2890cff49015d63476c482e142e4429f954
KirylLapouski <lapkovskyk@mail.ru>
2018-05-08 13:32:42 +0300

    Lesson ui

--- test-manager-api/client_src/src/components/Lesson.jsx
+++ test-manager-api/client_src/src/components/Lesson.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import UserInfo from './UserInfo';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import ExpansionPanel, {
+    ExpansionPanelSummary,
+    ExpansionPanelDetails,
+} from 'material-ui/ExpansionPanel';
+import Typography from 'material-ui/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 class Lesson extends React.Component {
 
     render() {
-        return(
-        <article className="z-depth-1" style={{marginTop:"20px",backgroundColor: "blue", backgroundImage:'url("https://lh4.googleusercontent.com/-64uhpsHBEZw/VMqrG_6wowI/AAAAAAAAAIE/_Pw_QoP0opU/w1005-h214-no/123_rainbowtriangle_teal.jpg")'}}>
-            <header style={{overflow:"hidden"}}>
-                <UserInfo disabled={true} style={{float:"left"}}/>
-                <Link to={"/lesson/"+this.props.id+"/topics"}>{this.props.title}</Link>                
-            </header>
-            <section>
-            </section>
-        </article>)
+        return (
+            <ExpansionPanel style={{ marginTop: "20px", backgroundColor: "blue", backgroundImage: 'url("https://lh4.googleusercontent.com/-64uhpsHBEZw/VMqrG_6wowI/AAAAAAAAAIE/_Pw_QoP0opU/w1005-h214-no/123_rainbowtriangle_teal.jpg")' }}>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon style={{ color: "white" }} />}>
+                    <UserInfo disabled={true} style={{ float: "left" }} />
+                    <Link to={"/lesson/" + this.props.id + "/topics"} style={{ height: "20px" }}>{this.props.title}</Link>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <Typography>
+                        {this.props.descrition}
+                    </Typography>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>)
     }
 }
 
 
 Lesson.propTypes = {
     title: PropTypes.string,
+    descrition: PropTypes.string,
     id: PropTypes.number.isRequired
 }
 
