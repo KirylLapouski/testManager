04a560fcfb888ced7099b09ff3d0fbfca5c23632
KirylLapouski <lapkovskyk@mail.ru>
2018-05-08 10:06:07 +0300

    signUp ui

---
+++ test-manager-api/client_src/src/components/SignUp.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import ReactRouterDom from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import toastr from 'toastr';
+import Flag from '@material-ui/icons/Flag';
+//CAN ADD REAL TIME VALIDATION
+class SignUp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            passwordConfirm: ''
+        };
+
+        this.onChangeHandler = this.onChangeHandler.bind(this);
+        this.onSubmitHandler = this.onSubmitHandler.bind(this);
+    }
+
+    onChangeHandler(e) {
+        var { name, value } = e.target;
+        this.setState(prevState => ({
+            [name]: value
+        }));
+    }
+    onSubmitHandler(e) {
+        e.preventDefault();
+
+        if (!this.state.email || !this.state.password || !this.state.passwordConfirm) {
+            toastr.error('Все поля должны быть заполнены');
+        } else if (this.state.password !== this.state.passwordConfirm) {
+            toastr.error('Пароли не совпадают');
+            //WRONG PASSWORD
+         }// else {
+
+        //     var xhr = new XMLHttpRequest();
+        //     xhr.open('POST', '/signUp/local', true);
+        //     xhr.setRequestHeader('Content-Type', 'application/json');
+        //     xhr.send(JSON.stringify({ email: this.state.email, password: this.state.password }));
+
+        //     xhr.onload = () => {
+        //         if (xhr.status == 200) {
+        //             xhr.open('GET', config.rootUrl + config.dbApi + "/getEnteredUser", true);
+        //             xhr.send();
+
+        //             xhr.onload = () => {
+        //                 if (xhr.status == 200) {
+        //                     localStorage.setItem("currentUser", xhr.responseText);
+        //                     document.location.href = "/user/" + JSON.parse(xhr.responseText)._id;
+        //                 }
+        //             }
+        //         } else {
+        //             toastr.error(xhr.responseText);
+        //             xhr.abort();
+        //         }
+        //     }
+        // }
+
+
+
+    }
+    render() {
+        return (
+            <div  style={{ backgroundImage: "url('https://mdbootstrap.com/img/Photos/Others/images/78.jpg')",height:"100vh", backgroundRepeat: "no-repeat", backgroundSize: "cover" }}>
+
+                <div className="w-100 h-100 mask rgba-black-light d-flex justify-content-center align-items-center">
+
+                    <div className="container" style={{ display: "flex", justifyContent: "center" }}>
+
+                        <div className="row wow fadeIn" style={{ visibility: "visible", animationName: "fadeIn" }}>
+                            <form name="signUp" method="POST" action='/signUp' style={{ width: "370px", padding: "30px 30px", borderRadius: "5px", backgroundColor: "#fff", color: "#4f4f4f",display:"flex",flexDirection:"column",alignItems:"flex-start" }}>
+                                <p className="h4 text-center mb-4" style={{alignSelf:"center"}}>Регистрация</p>
+
+
+                                <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">Электронная почта<Flag style={{color:"#ff7961",width:"14px",height:"14px"}}/></label>
+                                <input onChange={this.onChangeHandler} type="email" id="defaultFormRegisterEmailEx" name="email" className="form-control" />
+
+                                <br />
+
+                                <label htmlFor="defaultFormRegisterPasswordEx" className="grey-text">Пароль<Flag style={{color:"#ff7961",width:"14px",height:"14px"}}/></label>
+                                <input onChange={this.onChangeHandler} type="password" id="defaultFormRegisterPasswordEx" name="password" className="form-control" />
+
+                                <br />
+
+                                <label htmlFor="defaultFormRegisterCheckPassword" className="grey-text">Подвердите пароль<Flag style={{color:"#ff7961",width:"14px",height:"14px"}}/></label>
+                                <input onChange={this.onChangeHandler} type="password" id="defaultFormRegisterCheckPassword" name="passwordConfirm" className="form-control" />
+
+                                <div className="text-center mt-4" style={{alignSelf:"center"}}>
+                                    <button className="btn-primary btn" onClick={this.onSubmitHandler} type="submit" >Зарегистрироваться</button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>);
+    }
+}
+
+export default SignUp;   
