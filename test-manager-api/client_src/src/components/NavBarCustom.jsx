9ad2b6506b4d4c75719346cd4ba6177e01719c08
KirylLapouski <lapkovskyk@mail.ru>
2018-05-09 09:29:09 +0300

    plus button

--- test-manager-api/client_src/src/components/NavBarCustom.jsx
+++ test-manager-api/client_src/src/components/NavBarCustom.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, NavItem, NavLink } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
 import UserInfo from './UserInfo';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Add from '@material-ui/icons/Add';
+import Button from 'material-ui/Button';
+import Menu, { MenuItem } from 'material-ui/Menu';
 class NavBarCustom extends React.Component {
     constructor(props) {
         super(props);
@@ -10,11 +13,14 @@ class NavBarCustom extends React.Component {
         this.state = {
             collapse: false,
             isWideEnough: false,
-            dropdownOpen: false
+            dropdownOpen: false,
+            menu:null
         }
 
         //this.onClick = this.onClick.bind(this);
         this.toggle = this.toggle.bind(this);
+        this.handleMenuClick = this.handleMenuClick.bind(this);
+        this.handleMenuClose = this.handleMenuClose.bind(this)
     };
 
     handleClick() {
@@ -28,7 +34,15 @@ class NavBarCustom extends React.Component {
             dropdownOpen: !this.state.dropdownOpen
         })
     }
+
+    handleMenuClick(event){
+        this.setState({ menu: event.currentTarget });
+    }
+    handleMenuClose(){
+        this.setState({ menu: null });
+      };
     render() {
+        var {menu} = this.state;
         return (
             <Router>
                 <Navbar color="indigo" dark expand="md" scrolling>
@@ -36,11 +50,20 @@ class NavBarCustom extends React.Component {
                         <strong>NavBar</strong>
                     </NavbarBrand>
                     <NavbarNav left>
-                            <NavItem>
-                                <NavLink to="/cources">Home</NavLink>
-                            </NavItem>
+                        <NavItem>
+                            <NavLink to="/cources">Home</NavLink>
+                        </NavItem>
                     </NavbarNav>
                     <NavbarNav right>
+                        <NavItem style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                            <Button color="primary" style={{ color: "white" }} aria-label="add" aria-owns={menu ? 'simple-menu' : null} aria-haspopup="true" onClick={this.handleMenuClick}>
+                                <Add style={{ width: "30px", height: "30px" }} />
+                            </Button>
+                            <Menu id="simple-menu" anchorEl={menu} open={Boolean(menu)} style={{display:"relative",top:"40px"}} onClose={this.handleMenuClose}>
+                                <MenuItem onClick={this.handleClose}>Присоединиться</MenuItem>
+                                <MenuItem onClick={this.handleClose}>Создать курс</MenuItem>
+                            </Menu>
+                        </NavItem>
                         <NavItem>
                             <UserInfo />
                         </NavItem >
